54a24ba7ebf8ad6e2bb172e014659b681e5a4df7
r3tro-t3ch <joshi.vishnu1994@gmail.com>
2021-12-30 15:01:25 +0530

    exercise 4 completed

---
+++ exercise5/code/canvas/app.js
@@ -0,0 +1,16 @@
+// canvas 1
+let c = document.getElementById("canvas_1")
+let canvas = c.getContext("2d")
+canvas.strokeRect(20, 20, 150, 150) ;
+
+canvas.fillStyle = "#1d3557"
+canvas.fillRect(20,20, 150, 150);
+
+canvas.fillStyle = "#457b9d"
+canvas.fillRect(45, 45, 100, 100);
+
+canvas.fillStyle = "#a&dadc"
+canvas.fillRect(70, 70, 50,50);
+
+canvas.fillStyle="#f1faee"
+canvas.fillRect(90, 90, 10, 10);
